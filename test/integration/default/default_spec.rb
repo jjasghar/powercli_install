6ad45aedfcda3291096174803517d24eaaea64a1
JJ Asghar <jj@chef.io>
2018-07-19 10:27:34 -0500

    Added verbose linux checking
    Signed-off-by: JJ Asghar <jj@chef.io>

--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -12,7 +12,15 @@ describe port(443) do
   it { should_not be_listening }
 end
 
-if os.family == 'linux'
+if os.family == 'debian'
+  describe command('pwsh -Command "{& Get-InstalledModule -Name VMware.PowerCLI}"') do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should_not be_empty }
+  end
+  describe directory('/opt/microsoft/powershell') do
+    it { should exist }
+  end
+elsif os.family == 'redhat'
   describe command('pwsh -Command "{& Get-InstalledModule -Name VMware.PowerCLI}"') do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
